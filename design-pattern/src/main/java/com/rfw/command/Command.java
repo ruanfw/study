6d657a5ad4234f25260a82faa597518d9995e10c
ruanfw <ruanfw@yunbeitech.com>
2017-04-20 13:37:41 +0800

    init

---
+++ design-pattern/src/main/java/com/rfw/command/Command.java
@@ -0,0 +1,12 @@
+package com.rfw.command;
+
+/**
+ * Created by Administrator on 2017/4/20.
+ */
+public interface Command {
+
+    void execute();
+
+    void undo();
+
+}
---
+++ design-pattern/src/main/java/com/rfw/command/CreateCommand.java
@@ -0,0 +1,18 @@
+package com.rfw.command;
+
+/**
+ * Created by Administrator on 2017/4/20.
+ */
+public class CreateCommand implements Command {
+    public void execute() {
+
+    }
+
+    public void undo() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("11111111111");
+    }
+}
